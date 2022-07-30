76805759f2ad3c6a109cbf2d058c8ca319b2673b
Alhaimouni <m.alhaumouni@gmail.com>
2022-07-30 14:17:49 +0300

    Edit name

--- src/components/Header.js
+++ src/components/Header.js
@@ -104,7 +104,7 @@ class Header extends Component {
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
               <br/>
               <h1 className="mb-0">
-                {name}
+                Mohammad Al-Haimouni
               </h1>
               <div className="title-container">
                 <HeaderTitleTypeAnimation />
